08ba3fcde85d13f6d65bb258330b3f3bfa4d5e89
thepatrickniyo <patrickniyogitare28@gmail.com>
2025-10-31 22:45:23 +0200

    ft: added save prediction results & fetch lates entry endpoint

--- task_2_api/main.py
+++ task_2_api/main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
 from .mysql_routers import employees_router, departments_router, job_details_router 
-from .mongo_routers import mongo_departments_router, mongo_employees_router, mongo_job_details_router 
+from .mongo_routers import mongo_departments_router, mongo_employees_router, mongo_job_details_router, mongo_predictions_router
 # from mongo_database import mongo_db
 
 app = FastAPI(
     title="Employee Attrition API",
-    description="CRUD operations for employees, departments, and job details",
+    description="CRUD operations for employees, departments, job details, and predictions",
 )
 
 app.include_router(employees_router.router)
@@ -14,6 +14,7 @@ app.include_router(job_details_router.router)
 app.include_router(mongo_departments_router.router)
 app.include_router(mongo_employees_router.router)
 app.include_router(mongo_job_details_router.router)
+app.include_router(mongo_predictions_router.router)
 
 
 
